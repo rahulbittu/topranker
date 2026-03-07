8cfd423867b0aa0b34e5c9e56c8d570c4717de12
Rahul Pitta <rahulpitta@Rahuls-MBP.lan>
2026-03-07 11:46:34 -0600

    improve: add favicon and apple-touch-icon to web HTML head

--- app/+html.tsx
+++ app/+html.tsx
@@ -9,6 +9,8 @@ export default function Root({ children }: PropsWithChildren) {
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no, viewport-fit=cover" />
 
+        <link rel="icon" type="image/png" href="/assets/images/favicon.png" />
+        <link rel="apple-touch-icon" href="/assets/images/favicon.png" />
         <title>Top Ranker - Dallas Restaurant Rankings</title>
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
